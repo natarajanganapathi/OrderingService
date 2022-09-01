3419b7ba1cb634725ac84d95d5c2ac6ddd37bcdf
Natarajan Ganapathi <natarajanmca11@outlook.com>
2022-09-01 15:35:19 +0000

    web initial commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+
+
+# Reference:
+
+https://www.bing.com/videos/search?q=mediatr+c%23+tutorial&view=detail&mid=1C22CB32DCF2534E64D91C22CB32DCF2534E64D9&FORM=VIRE
---
+++ src/Web/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,57 @@
+import { Routes } from '@angular/router';
+
+import { DashboardComponent } from '../../dashboard/dashboard.component';
+import { CreateOrderComponent } from '../../create-order/create-order.component';
+import { OrdersComponent } from '../../orders/orders.component';
+
+export const AdminLayoutRoutes: Routes = [
+    // {
+    //   path: '',
+    //   children: [ {
+    //     path: 'dashboard',
+    //     component: DashboardComponent
+    // }]}, {
+    // path: '',
+    // children: [ {
+    //   path: 'userprofile',
+    //   component: UserProfileComponent
+    // }]
+    // }, {
+    //   path: '',
+    //   children: [ {
+    //     path: 'icons',
+    //     component: IconsComponent
+    //     }]
+    // }, {
+    //     path: '',
+    //     children: [ {
+    //         path: 'notifications',
+    //         component: NotificationsComponent
+    //     }]
+    // }, {
+    //     path: '',
+    //     children: [ {
+    //         path: 'maps',
+    //         component: MapsComponent
+    //     }]
+    // }, {
+    //     path: '',
+    //     children: [ {
+    //         path: 'typography',
+    //         component: TypographyComponent
+    //     }]
+    // }, {
+    //     path: '',
+    //     children: [ {
+    //         path: 'upgrade',
+    //         component: UpgradeComponent
+    //     }]
+    // }
+    { path: 'dashboard',      component: DashboardComponent },
+    { path: 'create-order',   component: CreateOrderComponent },
+    { path: 'orders',     component: OrdersComponent },
+    // { path: 'typography',     component: TypographyComponent },
+    // { path: 'icons',          component: IconsComponent },
+    // { path: 'maps',           component: MapsComponent },
+    // { path: 'notifications',  component: NotificationsComponent },
+];
